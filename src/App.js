c581da4bdb4cd6778e02d331d57c14a04ae847ce
himanshu sharma <53835288+honey829@users.noreply.github.com>
2021-05-02 11:57:04 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,121 @@
+import axios from "axios";
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [vUrl, setVUrl] = useState("");
+  const [file, setFile] = useState("");
+
+  const [lasturl, setLastUrl] = useState([]);
+  let suburl;
+  let binary = [];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    binary.push(file);
+    let blob = new Blob(binary, { type: "text/vtt" });
+    suburl = URL.createObjectURL(blob);
+
+    let url = {
+      Vurl: vUrl,
+      suburl: suburl
+    };
+
+    setLastUrl((prevState) => {
+      return [...prevState, url];
+    });
+  };
+
+  return (
+    <>
+      <div className="App">
+        <form
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignContent: "center"
+          }}
+        >
+          <div>
+            <label>Video URL </label>
+            <input
+              type="text"
+              placeholder="Enter video url"
+              onChange={(e) => {
+                e.preventDefault();
+                setVUrl(e.target.value);
+              }}
+              value={vUrl}
+            />
+          </div>
+          <div>
+            <label>Subtitles </label>
+            <input
+              placeholder="Subtitles"
+              type="file"
+              name="subtitle"
+              onChange={(e) => {
+                const formData = new FormData();
+                formData.append("Srt", e.target.files[0]);
+                axios
+                  .post(
+                    "https://efxvr.sse.codesandbox.io/uploadSrt",
+                    formData,
+                    {
+                      headers: {
+                        "content-type": "multipart/form-data"
+                      }
+                    }
+                  )
+                  .then((res) => {
+                    setFile(res.data);
+                    console.log(file);
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                  });
+              }}
+            />
+          </div>
+          <div>
+            <button
+              style={{
+                width: "20%"
+              }}
+              type="submit"
+              onClick={handleSubmit}
+            >
+              Submit
+            </button>
+          </div>
+        </form>
+        {lasturl.map((el, index) => {
+          return (
+            <li key={index}>
+              <p
+                style={{
+                  cursor: "pointer",
+                  color: "blueviolet",
+                  textDecorationLine: "underline"
+                }}
+              >
+                {el.Vurl}
+              </p>
+              <video id="video" width="500" height="400" controls>
+                <source src={el.Vurl} />
+                <track
+                  src={el.suburl}
+                  default
+                  srcLang="en"
+                  kind="captions"
+                  label="English"
+                />
+              </video>
+            </li>
+          );
+        })}
+      </div>
+    </>
+  );
+}
